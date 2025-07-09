d3cb3ac37c4d65f413e4cc77ff65b1050c811eca
Mark E Smith <mesmith@lx06.hep.ph.ic.ac.uk>
2025-07-09 17:41:12 +0100

    add scripts

---
+++ fixedTarget/bashScript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source /vols/lhcb/masmith/SHiP/FairShip/setUp.sh
+export ALIBUILD_WORK_DIR=/vols/lhcb/masmith/SHiP/FairShip/sw/
+alienv load FairShip/latest > test_config.sh
+source test_config.sh
+
+echo 'getting there'
+echo $FAIRSHIP
+
+echo 'INFO: Executing: python ' $@
+
+python /vols/lhcb/masmith/SHiP/FairShip/muonShieldOptimization/run_fixedTarget.py -n 10

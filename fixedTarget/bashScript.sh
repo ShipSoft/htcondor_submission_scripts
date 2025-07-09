c77dbe6ef9dacd42d5a26d34f79226395b7e3f2f
Mark E Smith <mesmith@lx06.hep.ph.ic.ac.uk>
2025-07-09 18:08:58 +0100

    tidy

--- fixedTarget/bashScript.sh
+++ fixedTarget/bashScript.sh
@@ -5,11 +5,11 @@ export ALIBUILD_WORK_DIR=/vols/lhcb/masmith/SHiP/FairShip/sw/
 alienv load FairShip/latest > test_config.sh
 source test_config.sh
 
-echo 'getting there'
-echo $FAIRSHIP
+echo 'INFO: Environment set up for FairShip located at ' $FAIRSHIP
 
 echo 'INFO: Executing: python /vols/lhcb/masmith/SHiP/FairShip/muonShieldOptimization/run_fixedTarget.py' $@
 
 python /vols/lhcb/masmith/SHiP/FairShip/muonShieldOptimization/run_fixedTarget.py $@
 
+echo 'INFO: Finished running. These files are on the WN:'
 ls -lh

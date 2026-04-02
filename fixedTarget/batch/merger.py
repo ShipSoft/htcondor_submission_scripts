3a8252d0b179e034ade2bebd56aaf0072a895466
Mark E Smith <mesmith@lx04.hep.ph.ic.ac.uk>
2026-04-02 14:59:23 +0100

    add merger script

---
+++ fixedTarget/batch/merger.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+import os
+import argparse
+import ROOT
+import uproot
+import json
+from datetime import datetime
+
+def mergeFiles(myfiles, tmpFile):
+    myChain = ROOT.TChain("cbmsim")
+    for _f in myfiles:
+        myChain.Add(_f)
+    myChain.Merge(tmpFile)
+
+
+parser = argparse.ArgumentParser(
+    description="Run FairShip file merger"
+)
+
+parser.add_argument("-i", "--inFiles", nargs="*",
+                    help="input files to merge")
+
+parser.add_argument("-n", "--genEvents",
+                    default=500000,
+                    type=int,
+                    help="How many events were generated to make this file"
+                    )
+
+parser.add_argument("-e", "--eCut",
+                    default=30,
+                    type=float,
+                    help="What was the energy cut to produce the file"
+                    )
+
+parser.add_argument("-j", "--gangaJob",
+                    type=int,
+                    help="Which ganga job produced these files",
+                    )
+parser.add_argument("-s", "--site",
+                    default="CERN",
+                    help="Which site was this run at"
+                    )
+
+parser.add_argument("-o", "--outputPath",
+                    help="A placeholder")
+
+parser.add_argument("--prodSite",
+                    default="CERN",
+                    help="Which site was this run at"
+                    )
+
+args = parser.parse_args()
+
+
+if args.prodSite == "GRIDPP":
+    os.environ["X509_USER_PROXY"]="/home/hep/mesmith/private/my_proxy.pem"
+    os.environ["X509_VOMSES"]="/cvmfs/grid.cern.ch/etc/grid-security/vomses"
+    os.environ["X509_VOMS_DIR"]="/cvmfs/grid.cern.ch/etc/grid-security/vomsdir"
+    os.environ["X509_CERT_DIR"]="/cvmfs/grid.cern.ch/etc/grid-security/certificates"
+
+files_to_merge = args.inFiles
+if not len(files_to_merge)>0:
+    print("ERROR: no files to merge! Doing nothing")
+    sys.exit()
+print("INFO: Merging %s files from job %s" % (len(files_to_merge), arg.gangaJob))
+
+total_pot = len(files_to_merge) * args.genEvents
+outName = f"pythia8_Geant4_eCut_{args.eCut}_PoT_{total_pot}_{args.site}_j{args.gangaJob}.root"
+
+mergeFiles(files_to_merge, outName)
+
+fsr = {
+    "PoT" : total_pot,
+    "EnergyCut": args.eCut,
+    "PoTperFile": args.genEvents,
+    "nFilesMerged": len(files_to_merge),
+    "filesMerged": args.inFiles,
+    "prodSite": args.prodSite,
+    "mergeDate": datetime.today().strftime('%Y-%m-%d')
+}
+
+print("INFO: Adding the file summary")
+with uproot.update(outName) as _f:
+    _f["FileSummary"] = json.dumps(fsr)
+
+print("INFO: All done")
--- fixedTarget/batch/wn_script.py
+++ fixedTarget/batch/wn_script.py
@@ -60,7 +60,8 @@ def main():
     FS_INSTALL = args.fs_install or '/cvmfs/ship.cern.ch/' + args.cvmfs_version
     WORK_DIR = args.work_dir or f"{FS_INSTALL}/sw/"
     INIT_SCRIPT = args.init_script or f"{FS_INSTALL}/sw/slc9_x86-64/FairShip/latest/etc/profile.d/init.sh"
-    RUN_SCRIPT = f"{FS_INSTALL}/sw/slc9_x86-64/FairShip/latest/macro/{args.runfile}"
+#    RUN_SCRIPT = f"{FS_INSTALL}/sw/slc9_x86-64/FairShip/latest/macro/{args.runfile}"
+    RUN_SCRIPT=args.runfile
 
     fs_version = args.cvmfs_version
     fs_tag = args.FairShip_tag or args.cvmfs_version
@@ -79,6 +80,8 @@ def main():
     command = f"""
     export WORK_DIR="{WORK_DIR}"
     source "{INIT_SCRIPT}"
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
+    
     python "{RUN_SCRIPT}" {passthrough}
     """
 
